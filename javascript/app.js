019e5ef8d022edeb1f90e2958d2c69792290f7d5
pourya <pourya.gh095@gmail.com>
2021-06-02 01:54:53 +0430

    giveh1

--- javascript/app.js
+++ javascript/app.js
@@ -1,6 +1,14 @@
+
 /**** make mother div ****/
 const motherDiv = document.createElement("div")
 motherDiv.classList = "container"
+motherDiv.style.backgroundColor = "red"
+motherDiv.style.height = "676px"
+motherDiv.style.width = "100%"
+motherDiv.style.display = "flex"
+motherDiv.style.justifyContent = "center"
+motherDiv.style.alignItems = "center"
+
 document.body.append(motherDiv)
 
 
@@ -8,12 +16,20 @@ document.body.append(motherDiv)
 
 /****  creat form tag****/
 const myForm = document.createElement("form")
+myForm.style.height ="300px"
+myForm.style.width = "200px"
+myForm.style.backgroundColor = "blue"
+myForm.style.justifyContent = "center"
 motherDiv.append(myForm)
-console.log(motherDiv)
 
+/**** creat h1(log in) and append to fotm tag ****/
+const myH = document.createElement("h1")
+// myH.valiu = "log in"
+myForm.append(myH)
 
 /**** creat div and username and append to form ****/
 const divUserName = document.createElement("div")
+divUserName.style.margin = "4px"
 const userName = document.createElement("input")
 userName.type = "search"
 userName.placeholder = "user name"
@@ -22,6 +38,7 @@ myForm.append(divUserName)
 
 /**** creat div and password and append to form ****/
 const divPassword = document.createElement("div")
+divPassword.style.margin = "4px"
 const myPassword = document.createElement("input")
 myPassword.type = "password"
 myPassword.placeholder = "password"
@@ -29,4 +46,6 @@ divPassword.append(myPassword)
 myForm.append(divPassword)
 
 /*** creat button and append to form ***/
-const myButt
+const mySubMit = document.createElement("input")
+mySubMit.type = "submit"
+myForm.append(mySubMit)
