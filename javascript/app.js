323196aeb85f9d189a3ccbee532c1c98426fc6e2
pourya <pourya.gh095@gmail.com>
2021-06-03 18:03:27 +0430

    normal-2

--- javascript/app.js
+++ javascript/app.js
@@ -2,7 +2,7 @@
 /**** make mother div ****/
 const motherDiv = document.createElement("div")
 motherDiv.classList = "container"
-motherDiv.style.backgroundColor = "black"
+motherDiv.style.backgroundColor = "#a1a1a1"
 motherDiv.style.height = "676px"
 motherDiv.style.width = "100%"
 motherDiv.style.display = "flex"
@@ -19,22 +19,20 @@ document.body.append(motherDiv)
 /****  creat form tag****/
 const myForm = document.createElement("form")
 myForm.style.height ="430px"
-myForm.style.width = "400px"
-myForm.style.backgroundColor = "blue"
+myForm.style.width = "350px"
+myForm.style.backgroundColor = "black"
 myForm.style.textAlign = "center"
 myForm.style.display = "flex"
 myForm.style.flexDirection = "column"
-// myForm.style.alignItems = "flex-start"
-// myForm.style.justifyContent = "center"
 myForm.style.alignItems = "center"
 
-// myForm
+myForm.style.boxShadow = "-5px -5px 10px rgba(255,255,255,0.1),5px 5px 10px rgb(0,0,0)"
 // myForm.style.alignContent = "center"
 motherDiv.append(myForm) 
 
 /**** creat h1(log in) and append to fotm tag ****/
 const myH = document.createElement("h1")
-// myH.style.textAlign = "center"
+myH.style.color= "#fff"
 myH.innerHTML ="Log in"
 myForm.append(myH)
 
@@ -46,7 +44,11 @@ divUserName.style.height ="5px"
 const userName = document.createElement("input")
 userName.type = "search"
 userName.placeholder = "user name"
-userName.style.width = "208px"
+userName.style.width = "205px"
+userName.style.height = "25px"
+userName.style.outline="0px"
+userName.style.borderRadius ="6px" 
+userName.style.border = "none"
 divUserName.append(userName)
 myForm.append(divUserName)
 
@@ -57,6 +59,10 @@ const myPassword = document.createElement("input")
 myPassword.type = "password"
 myPassword.placeholder = "password"
 myPassword.style.width = "200px"
+myPassword.style.height = "24px"
+myPassword.style.outline="none"
+myPassword.style.border ="none" 
+myPassword.style.borderRadius ="6px" 
 divPassword.append(myPassword)
 myForm.append(divPassword)
 
@@ -64,11 +70,12 @@ myForm.append(divPassword)
 const mySubMit = document.createElement("input")
 
 mySubMit.type = "submit"
-// mySubMit.style.alignItems = "center"
-mySubMit.style.width = "200px"
-mySubMit.style.height = "20px"
+mySubMit.value = "Log in"
+mySubMit.style.backgroundColor = "rgb(230)"
+mySubMit.style.width = "208px"
+mySubMit.style.height = "25px"
 mySubMit.style.borderRadius = "10px"
-mySubMit.style.border = "red"
+mySubMit.style.border = "0px"
 mySubMit.style.margin= "20px"
 
 
